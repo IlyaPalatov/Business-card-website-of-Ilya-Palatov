94f2f822938312d02df21d17d47fa6cf6b9065ad
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2024-10-24 17:43:43 +0300

    added-new-styles-for-filter-and-cards-also-added-styles-for-contact-me-modal

--- src/components/ModalContactToMe.jsx
+++ src/components/ModalContactToMe.jsx
@@ -9,11 +9,26 @@ const Modal = ({ isOpen, onClose, children }) => {
             onClose(); 
         }
     };
+    const handleBtnCloseClick = (event) => {
+        if (event.currentTarget === event.target) {
+            onClose();
+        }
+    }
 
     return (
         <div className="overlay-modal" onClick={handleOverlayClick}>
             <div className="modal-bg">
-                
+                <div><button onClick={handleBtnCloseClick}>Close</button></div>
+                <div><h3 className='header-of-modal'>Let's Talk!</h3></div>
+                <div>
+                    <form action="POST">
+                        <input className='modal-input' placeholder='enter' type="text" />
+                        <input className='modal-input' placeholder='enter' type="text" />
+                        <input className='modal-input' placeholder='enter' type="text" />
+                        <input className='modal-input' placeholder='enter' type="text" />
+                    </form>
+                </div>
+                <div><button onClick={handleBtnCloseClick} className='modal-btn-send'>Send</button></div>
                 {children}
             </div>
         </div>
